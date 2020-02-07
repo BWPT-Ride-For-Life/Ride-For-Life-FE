f051dcd636fb03873c42fe2d5dfa842d4a7626c5
amberm <nekosenpai@gmail.com>
2020-02-06 20:32:22 -0700

    added Joe and Sheila changes from previous Gits

--- src/components/DriverRegistration.js
+++ src/components/DriverRegistration.js
@@ -1,240 +1,163 @@
-import React, { Component, useState} from "react";
-import { Redirect, Link }  from 'react-router-dom'
-import '../App.css';
-import {axiosWithAuth} from "../utils/AxiosWithAuth";
-
-function emailIsValid (email) {
-    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
-}
-const formValid = ({formErrors, ...rest}) => {
-    let valid = true;
-//This validates empty form errors
-    Object.values(formErrors).forEach(val => {
-        val.length > 0 && (valid = false);
-    Object.values(rest).forEach(val =>{
-        val === null && (valid = false)
-    });
-    });
-    return valid;
-};
-//This validates filled forms
-
-// const [redirect, setRedirect] = useState(false)
-
-export default class DriverRegistration extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            firstName: null,
-            lastName: null,
-            location_id: null,
-            price: null,
-            email: null,
-            phoneNumber: null,
-            password: null,
-            redirect: false,
-            formErrors: {
-                firstName: "",
-                lastName: "",
-                location_id: "",
-                price: "",
-                email: "",
-                phoneNumber: "",
-                password: ""
-            }
-        };
-
+import React, { Component } from "react";
+//import './App.css';
+export default class DriverRegistrationForm extends React.Component{
+    constructor (props){
+      super(props)
+      this.state ={
+        firstName: "",
+        lastName: "",
+        location: "",
+        price: "''",
+        email: "''",
+        phoneNumber: '',
+        password: '',
+            firstNameError: "",
+            lastNameError: "",
+            locationError: "",
+            priceErrorError: "",
+            emailError: "",
+            phoneNumberError: "",
+            passwordError: ""  
+      };
     }
-    handleSubmit = e => {
-        e.preventDefault();
-
-        if (formValid(this.state)) {
-            console.log(`
-        --SUBMITTING--
-        First Name: ${this.state.firstName}
-        Last Name: ${this.state.lastName}
-        Location: ${this.state.location_id}
-        Price: ${this.state.price}
-        Email: ${this.state.email}
-        Phone Number: ${this.state.phoneNumber}
-        Password: ${this.state.password}
-        `)
-            let info = {
-                firstName: this.state.firstName,
-                lastName: this.state.lastName,
-                location_id: this.state.location_id,
-                price: this.state.price,
-                email: this.state.email,
-                phoneNumber: this.state.phoneNumber,
-                password: this.state.password
-            }
-        axiosWithAuth()
-            .post('api/auth/register-driver', info)
-            .then(res => {
-                localStorage.setItem('token', res.data.token)
-                localStorage.setItem('id', res.data.id)
-                console.log(res)
-                this.setState({
-                    redirect: true
-                })
-                // eslint-disable-next-line no-undef
-                // if (redirect === true) {
-                //
-                // }
-            })
-            .catch(error => console.log(error, "Login Error"))
-        } else {
-            console.error('FORM INVALID - ERROR')
-        }
+    
+  
+    handleChange = (event) =>{
+      this.setState({
+          [event.target.name]: event.target.value});
+      }
+  
+    validate =()=>{
+   let firstNameError = "";
+    let lastNameError = "";
+    // let locationError = "";
+    // let priceError = "";
+    let emailError = "";
+    // let phoneNumberError = "";
+    // let passwordError = "";
+    if (!this.state.firstName){
+      firstNameError = '';
     }
-    renderRedirect = () => {
-        if (this.state.redirect) {
-            return <Redirect to='DriverAccount'/>
-        }
+    if (!this.state.lastName){
+      lastNameError = '';
     }
-    handleChange = e => {
-        e.preventDefault();
-        const { name, value } = e.target;
-        let formErrors = this.state.formErrors;
-
-        switch (name) {
-            case 'firstName':
-                formErrors.firstName = value.length < 3  ? 'minimum 3 characters required' : '';
-                break;
-            case 'lastName':
-                formErrors.lastName = value.length < 3  ? 'minimum 3 characters required' : '';
-                break;
-            case 'location':
-                formErrors.location_id = value.length < 1 ? 'minimum 10 characters required' : '';
-                break;
-            case 'price':
-                formErrors.price = value.length < 6 ? 'minimum 6 characters required' : '';
-                break;
-            case 'email':
-                formErrors.email = emailIsValid(value) ? '' : 'email invalid';
-                break;
-            case 'phoneNumber':
-                formErrors.phoneNumber = value.length < 10 ? 'minimum 11 characters required' : '';
-                break;
-            case 'password':
-                formErrors.password = value.length < 8 ? 'minimum 8 characters required' : '';
-                break;
-            default:
-                break;
-        }
-
-        this.setState({formErrors, [name]: value}, () => console.log(this.state))
-
-    };
-
+    if (!this.state.email.includes('@')){
+      emailError = 'invalid email';
+    }
+    if (emailError || firstNameError ||lastNameError){
+      this.setState({emailError, firstNameError, lastNameError});
+      return false;
+    }
+    return true;
+  };
+    handleSubmit = event => {
+        event.preventDefault();
+        const isValid = this.validate();
+        if (isValid){
+          console.log(this.state);
+       //clear form 
+  
+      }
+    }
+  
+  
+        submit(){
+          let obj={}
+        obj.firstame= this.state.firstName;
+        obj.lastame= this.state.lastName;
+        obj.location= this.state.location;
+        obj.price=this.state.price;
+        obj.email=this.state.email;
+        obj.firstame=this.state.phoneNumber;
+        obj.firstame=this.state.password;
+        
+    }
+    
+      
     render() {
-
-        const {formErrors} = this.state;
         
         return <div className='wrapper'>
-            {this.renderRedirect()}
             <div className='form-wrapper'>
-                <h1>Rider Account</h1>
-                <form onSubmit={this.handleSubmit} noValidate>
+                <h1>Driver Account</h1>
+                <form onSubmit={this.handleSubmit}>
                     <div className='firstName'>
                         <label htmlFor='firstName'>First Name</label>
                         <input
-                            type='text'
-                            className={formErrors.firstName.length > 0 ? 'error' : null}
-                            placeholder='First Name'
                             name='firstName'
-                            noValidate
-                            onChange={this.handleChange} />
-                            {formErrors.firstName.length > 0 && (
-                                <span className='errorMessage'>{formErrors.firstName}</span>
-                            )}
+                            placeholder='First Name'
+                            type='firstName'
+                            firstname='firstName'
+                           onChange= {this.handleChange} />
+                        <div style={{fontSize: 10, color: 'red'}}>{this.state.firstNameError}</div>    
                     </div>
                     <div className='lastName'>
                         <label htmlFor='lastName'>Last Name</label>
                         <input
-                            className={formErrors.lastName.length > 0 ? 'error' : null}
+                            name='lastName'
                             placeholder='Last Name'
                             type='lastName'
-                            name='lastName'
-                            noValidate
-                            onChange={this.handleChange} />
-                            {formErrors.lastName.length > 0 && (
-                                <span className='errorMessage'>{formErrors.lastName}</span>
-                            )}
+                            lastname='lastName'
+                            onChange={this.handleChange} />         
                     </div>
-                    <div className='location'>
+                    <div className='location_id'>
                         <label htmlFor='location_id'>Location</label>
-                        <input
-                            className={formErrors.location_id.length > 0 ? 'error' : null}
-                            placeholder='location_id'
-                            type='number'
-                            name='location_id'
-                            noValidate
-                            onChange={this.handleChange} />
-                        {formErrors.location_id.length > 0 && (
-                            <span className='errorMessage'>{formErrors.location_id}</span>
-                        )}
+                        <select>
+  <option selected value="Select One">Select One</option>
+  <option value="id_1">Kampala</option>
+  <option value="id_2">Nansana</option>
+  <option value="id_3">Kira</option>
+</select>  
+    {this.handleChange} 
+                            
                     </div>
                     <div className='price'>
                         <label htmlFor='price'>Price</label>
                         <input
-                            className={formErrors.price.length > 0 ? 'error' : null}
-                            placeholder='Price'
-                            type='price'
                             name='price'
-                            noValidate
+                            placeholder='$Price'
+                            type='price'
+                            email='price'
                             onChange={this.handleChange} />
-                        {formErrors.price.length > 0 && (
-                            <span className='errorMessage'>{formErrors.price}</span>
-                        )}
+                                
                     </div>
                     <div className='email'>
                         <label htmlFor='email'>Email</label>
                         <input
-                            className={formErrors.email.length > 0 ? 'error' : null}
+                            name='email'
                             placeholder='Email'
                             type='email'
-                            name='email'
-                            noValidate
+                            email='email'
                             onChange={this.handleChange} />
-                            {formErrors.email.length > 0 && (
-                                <span className='errorMessage'>{formErrors.email}</span>
-                            )}
+                                
                     </div>
                     <div className='phoneNumber'>
                         <label htmlFor='phoneNumber'>Phone Number</label>
                         <input
-                            className={formErrors.phoneNumber.length > 0 ? 'error' : null}
+                            name='phoneNumber'
                             placeholder='Phone Number'
                             type='phoneNumber'
-                            name='phoneNumber'
+                            phoneNumber='phoneNumber'
                             noValidate
                             onChange={this.handleChange} />
-                            {formErrors.phoneNumber.length > 0 && (
-                                <span className='errorMessage'>{formErrors.phoneNumber}</span>
-                            )}
+                               
                     </div>
                     <div className='password'>
                         <label htmlFor='password'>Password</label>
                         <input
-                            className={formErrors.password.length > 0 ? 'error' : null}
+                            className='password'
                             placeholder='Password'
                             type='password'
                             name='password'
-                            noValidate
                             onChange={this.handleChange} />
-                            {formErrors.password.length > 0 && (
-                                <span className='errorMessage'>{formErrors.password}</span>
-                            )}
+                                 
+                            
+                            
                     </div>
                     <div className='createAccount'>
-                        <button type='submit' /*onClick={this.setRedirect}*/>Create Account</button>
+                        <button onClick={() => this.submit()}type='submit'>Submit</button>
                         <small>Already Have an Account?</small>
-                        <Link to='/login'>Click here</Link>
                     </div>
                 </form>
-
             </div>
         </div>;
     }
